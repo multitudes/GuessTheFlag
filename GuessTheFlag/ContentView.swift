256f5a56616dc72c2cb772bb4ee0c200e9b1c4a1
laurent <wrmultitudes@gmail.com>
2019-10-28 15:08:00 +0100

    done

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,18 +11,19 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
-    @State private var scoreTitle = "Lets start!"
-
+    @State private var scoreTitle = "Ready!"
+    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var score = 0
-
+    
     @State private var flagOpacity = [1.0, 1.0, 1.0]
     @State private var flagBlur: [CGFloat] = [0, 0, 0]
     @State private var animationAmount: [CGFloat] = [1.0, 1.0, 1.0]
     @State var allowhittesting = true
-    var showMessage: String {
+    @State private var flagAngle = [0.0, 0.0, 0.0]
+        var showMessage: String {
         return scoreTitle
     }
     
@@ -49,10 +50,12 @@ struct ContentView: View {
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of").foregroundColor(.white)
+                    Spacer(minLength: 10)
                     Text(countries[correctAnswer])
-                        .font(.largeTitle)
-                        .fontWeight(.black)
+                        .lineLimit(nil)
                         .foregroundColor(.white)
+                        .font(.largeTitle)
+                        
                 }
                 
                 ForEach(0 ..< 3) { number in
@@ -60,14 +63,15 @@ struct ContentView: View {
                         self.flagTapped(number)
                     }) {
                         FlagImage(country: self.countries[number])
-
+                        
                     }
                     .opacity(self.flagOpacity[number])
+                    .rotation3DEffect(.degrees(self.flagAngle[number]), axis: (x: 1, y: 1, z: 0))
                     .blur(radius: self.flagBlur[number])
                     .scaleEffect(self.animationAmount[number])
                     .allowsHitTesting(self.allowhittesting ? true : false)
-                    .animation(.default)
-                     }
+                    .animation(.easeOut)
+                }
                 
                 
                 Text("current score \(score)")
@@ -77,25 +81,20 @@ struct ContentView: View {
                     .font(.title)
                     .lineLimit(2)
                     .multilineTextAlignment(.center)
-                Text("Details go here.")
-                .transition(.move(edge: .bottom))
+                
                 Spacer()
             }
-            //            .alert(isPresented: $showingScore) {
-            //                Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
-            //                    self.askQuestion()
-            //
-            //                    })
-            //            }
-        } 
+        }
     }
     func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        scoreTitle = "Ready!"
+        flagAngle = [0.0, 0.0, 0.0]
         animationAmount = [1.0, 1.0, 1.0]
         flagOpacity = [1.0, 1.0, 1.0]
         flagBlur = [0, 0, 0]
         allowhittesting = true
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
     func flagTapped(_ number: Int) {
         allowhittesting = false
@@ -109,7 +108,7 @@ struct ContentView: View {
             Wrong!
             That was \(countries[number])!
             """
-            wrongAnimation()
+            wrongAnimation(number)
         }
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             self.askQuestion()
@@ -122,16 +121,20 @@ struct ContentView: View {
         for flag in 0...2 {
             if flag == correctAnswer {
                 animationAmount[flag] = 1.40
+                flagAngle[flag] = 360.0
             } else {
-                flagOpacity[flag] = 0.25
+                flagOpacity[flag] = 0
             }
         }
     }
     
-    func wrongAnimation() {
+    func wrongAnimation(_ number: Int) {
         for flag in 0...2 {
-            if flag != correctAnswer {
-                flagBlur[flag] = 6
+            if flag == number {
+                flagBlur[flag] = 0.5
+                animationAmount[flag] = 1.40
+            } else {
+                flagOpacity[flag] = 0
             }
         }
     }
